e408ea6852596a4863be7827c586dff59792a611
elmagnifiko <abdoulrazakadamoubello@gmail.com>
2024-05-11 13:11:30 +0100

    create-user-files-in-ctrl-&-models

--- app.js
+++ app.js
@@ -2,19 +2,23 @@ const express = require("express");
 const mongoose = require('mongoose');
 const app = express();
 const stuffRoutes = require('./routes/stuff');
+const userRoutes = require('./routes/user');
 mongoose.connect('mongodb+srv://Raz:1234mongo@bac.ugwmoa7.mongodb.net/?retryWrites=true&w=majority&appName=BAC',
-  { useNewUrlParser: true,
-    useUnifiedTopology: true })
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-  });
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
 
-  app.use(express.json());
-  app.use('/api/stuff', stuffRoutes);
-module.exports = app ;
+app.use(express.json());
+app.use('/api/stuff', stuffRoutes);
+app.use('/api/auth', userRoutes);
+module.exports = app;
