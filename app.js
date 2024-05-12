74dde136224e85eefefcadf195a2b5e029eab5f1
elmagnifiko <abdoulrazakadamoubello@gmail.com>
2024-05-12 19:57:54 +0100

    update-createThing-routes-&-app

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const app = express();
 const stuffRoutes = require('./routes/stuff');
 const userRoutes = require('./routes/user');
+const path = require('path');
 mongoose.connect('mongodb+srv://Raz:1234mongo@bac.ugwmoa7.mongodb.net/?retryWrites=true&w=majority&appName=BAC',
   {
     useNewUrlParser: true,
@@ -21,4 +22,5 @@ app.use((req, res, next) => {
 app.use(express.json());
 app.use('/api/stuff', stuffRoutes);
 app.use('/api/auth', userRoutes);
+app.use('/images', express.static(path.join(__dirname, 'images')));
 module.exports = app;
